847b0d505f51f8b4eb656f6730ee62a07f296af7
raghuchandra <raghuchandrakanakamedala@gmail.com>
2025-11-01 15:19:16 +0530

    node_module removed

--- background.js
+++ background.js
@@ -1,26 +1,3 @@
-//chrome.sidePanel
-//  .setPanelBehavior({ openPanelOnActionClick: true })
-//  .catch((error) => console.error(error));
-//
-//chrome.tabs.onActivated.addListener((activeInfo) => {
-//  showSummary(activeInfo.tabId);
-//});
-//chrome.tabs.onUpdated.addListener(async (tabId) => {
-//  showSummary(tabId);
-//});
-//
-//async function showSummary(tabId) {
-//  const tab = await chrome.tabs.get(tabId);
-//  if (!tab.url.startsWith('http')) {
-//    return;
-//  }
-//  const injection = await chrome.scripting.executeScript({
-//    target: { tabId },
-//    files: ['scripts/extract-content.js']
-//  });
-//  chrome.storage.session.set({ pageContent: injection[0].result });
-//}
-
 // Open the extension in a new tab when the extension icon is clicked
 chrome.action.onClicked.addListener((activeTab) => {
   showSummary(activeTab.id);
